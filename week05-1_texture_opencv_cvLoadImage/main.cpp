c4fe0d03a8d52d46da2b314d4ab065928e05a951
LeeTShawn <112918703+LeeTShawn@users.noreply.github.com>
2024-03-18 16:41:01 +0800

    week05

---
+++ week05-1_texture_opencv_cvLoadImage/main.cpp
@@ -0,0 +1,18 @@
+#include <GL/glut.h>
+#include <opencv/highgui.h>
+void display()
+{
+    glutSolidTeapot( 0.3 );
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+    IplImage * img = cvLoadImage("c:/chiwawa.png");
+    cvShowImage("opencv", img);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week05-1_texture_opencv_cvLoadImage");
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
